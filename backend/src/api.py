9deef2c1d91a36f4650064a72c79596ed98a8a25
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-09-01 23:49:24 -0700

    Add create drink endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -79,6 +79,26 @@ def get_drinks():
 '''
 
 
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink():
+      body = request.get_json()
+      title = body.get('title')
+      recipe = body.get('recipe')
+    try:
+        drink = Drink(title=title, recipe=json.dumps(recipe))
+        drink.insert()
+
+        return jsonify({
+          'success': True,
+          'drinks': drink.long()
+        })
+
+    except Exception as e:
+      abort(422)
+
+
+
 '''
 @TODO implement endpoint
     PATCH /drinks/<id>
