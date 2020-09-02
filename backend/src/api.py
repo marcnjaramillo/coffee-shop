fcece39b0475378ce089211ec5fd73a522ad4534
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-09-02 01:21:06 -0700

    Add patch endpoint for drinks

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,6 +11,13 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
 '''
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
@@ -53,34 +60,44 @@ def get_drinks():
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def create_drink():
-      body = request.get_json()
-      title = body.get('title')
-      recipe = body.get('recipe')
+    body = request.get_json()
+    title = body.get('title')
+    recipe = body.get('recipe')
     try:
         drink = Drink(title=title, recipe=json.dumps(recipe))
         drink.insert()
 
         return jsonify({
-          'success': True,
-          'drinks': drink.long()
+            'success': True,
+            'drinks': drink.long()
         })
 
     except Exception as e:
-      abort(422)
+        print(e)
+        abort(422)
 
 
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(id):
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+    if not drink:
+        abort(404)
+
+    try:
+        body = request.get_json()
+        drink.title = body.get('title', drink.title)
+        drink.recipe = json.dumps(body.get('recipe'))
+        drink.update()
+
+        return jsonify({
+            'success': True,
+            'drinks': drink.long()
+        })
+    except Exception as e:
+        print(e)
+        abort(422)
 
 
 '''
@@ -96,38 +113,20 @@ def create_drink():
 
 
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-
 
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
         "success": False,
         "error": 422,
-        "message": "unprocessable"
+        "message": "Unprocessable"
     }), 422
 
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource not found"
+    }), 404
