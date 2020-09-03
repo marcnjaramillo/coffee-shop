e547fc82d000b61e3c2d45763c923bf17e1eb2b2
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-09-02 23:09:28 -0700

    Add delete endpoint and update protected endpoints

--- backend/src/api.py
+++ backend/src/api.py
@@ -44,7 +44,7 @@ def get_drinks():
 
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
-def get_drinks():
+def get_drinks(payload):
     all_drinks = Drink.query.all()
     drinks = [drink.long() for drink in all_drinks]
 
@@ -59,7 +59,7 @@ def get_drinks():
 
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
-def create_drink():
+def create_drink(payload):
     body = request.get_json()
     title = body.get('title')
     recipe = body.get('recipe')
@@ -79,10 +79,10 @@ def create_drink():
 
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
-def update_drink(id):
+def update_drink(payload, id):
     drink = Drink.query.filter(Drink.id == id).one_or_none()
 
-    if not drink:
+    if drink is None:
         abort(404)
 
     try:
@@ -100,19 +100,29 @@ def update_drink(id):
         abort(422)
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    try:
+        drink = Drink.query.filter(
+            Drink.id == id).one_or_none()
 
+        if drink is None:
+            abort(404)
+
+        drink.delete()
+
+        return jsonify({
+            'success': True,
+            'delete': id
+        })
+
+    except Exception as e:
+        print(e)
+        abort(422)
 
 # Error Handling
+
 
 @app.errorhandler(422)
 def unprocessable(error):
